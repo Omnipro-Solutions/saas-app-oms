1484abf5d8379e7c31239d76eea1f614e3d281d5
Omni-Stiven <stiven.pinzon@omni.pro>
2024-02-02 17:21:32 -0500

    refactor: updating operation model

--- omni_pro_oms/models/operation.py
+++ omni_pro_oms/models/operation.py
@@ -4,15 +4,24 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 from omni_pro_base.models import OmniModel
 
+SCORE_CHOICES = (
+    ("low", "Low"),
+    ("medium", "Medium"),
+    ("high", "High"),
+    ("critical", "Critical"),
+)
+
 
 class Operation(OmniModel):
     name = models.CharField(max_length=255, verbose_name=_("Name"))
     destination = models.CharField(max_length=255, verbose_name=_("Destination"))
-    score = models.IntegerField(verbose_name="Score")
+    score = models.CharField(
+        choices=SCORE_CHOICES, max_length=255, verbose_name=_("Score"), help_text=_("Level of priority")
+    )
     endpoint_url = models.CharField(max_length=255, verbose_name=_("Endpoint URL"))
     http_method = models.CharField(max_length=255, verbose_name=_("HTTP Method"))
-    timeout = models.IntegerField(verbose_name=_("Timeout"))
-    auth_type = models.CharField(max_length=255, verbose_name=_("Auth Type"))
+    timeout = models.IntegerField(verbose_name=_("Timeout"), help_text=_("In seconds"))
+    auth_type = models.CharField(max_length=255, verbose_name=_("Auth Type"), help_text=_("Basic, Bearer, etc."))
     headers = models.JSONField(verbose_name=_("Headers"), blank=True, null=True)
 
     history = AuditlogHistoryField()
