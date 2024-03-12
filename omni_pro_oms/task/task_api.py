e4e57d1f84e19f5c6008aeb903dd47a31043961a
scastillo <saulo.castillo@omni.pro>
2024-03-12 16:28:27 -0500

    call_oms_api in utils

--- omni_pro_oms/task/task_api.py
+++ omni_pro_oms/task/task_api.py
@@ -5,42 +5,27 @@ from omni_sdk_oms.openapi_client.models.user_token import UserToken
 from omni_sdk_oms.openapi_client.api_client import ApiClient
 from omni_sdk_oms.openapi_client.configuration import Configuration
 from urllib3 import HTTPResponse
+from omni_pro_oms import utils
 import requests
+import urllib3
 
 
 class TaskApi:
-    def __init__(self, task: Task) -> None:
+
+    def __init__(self, task: Task, timeout=30) -> None:
         self.task = task
         self.oms_api = DefaultApi()
         self.token = None
         self.tenant = self.task.tenant_id
+        self.timeout = timeout
 
     def _set_oms_api_token(self):
-        user_token = UserToken(
-            client_id=self.tenant.client_id,
-            client_secret=self.tenant.client_secret,
-            tenant=self.tenant.code,
-        )
-        res = self.oms_api.api_v1_auth_token_post(user_token)
-        self.token = res.get("authentication_result", {}).get("token")
+        self.token = utils.get_oms_auth_token(self.tenant)
 
-    def call_api_oms(self, endpoint: str, http_method: str, **kwargs) -> dict:
+    def call_oms_api(self, method: str, endpoint, **kwargs) -> dict:
         if not self.token:
             self._set_oms_api_token()
-
-        if self.token:
-            headers = {"Accept": "application/json", "Authorization": self.token}
-            url = f"{self.oms_api.api_client.configuration.host}{endpoint}"
-
-            if http_method == "GET":
-                print(kwargs)
-                kwargs["headers"] = headers
-                return requests.request(method=http_method, url=url, **kwargs).json()
-            else:
-                kwargs.update({"header_params": headers})
-                return self.oms_api.api_client.call_api(
-                    http_method, url, **kwargs
-                ).response.json()
-
-        else:
-            raise Exception("Undefined token")
+        kwargs.update({"timeout": self.timeout})
+        return utils.call_oms_api(
+            method=method, endpoint=endpoint, token=self.token, **kwargs
+        )
