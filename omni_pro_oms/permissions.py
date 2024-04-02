7a023b11db23f0af19f0bf17317aaa5a3dbc2631
Edgar Elias Peña De La Torre <edgar.pena@omni.pro>
2024-04-01 19:39:00 -0500

    Replace TokenHasReadWriteScope with TokenValidScope in OMNIAPIView Add TokenValidScope permission class to OMNIAPIView

---
+++ omni_pro_oms/permissions.py
@@ -0,0 +1,18 @@
+import requests
+from django.conf import settings
+from rest_framework import permissions
+
+
+class TokenValidScope(permissions.BasePermission):
+    message = "Invalid or expired token."
+
+    def has_permission(self, request, view):
+        auth = request.headers.get("Authorization", None)
+        if auth:
+            token = auth.split(" ")[-1]
+            url = settings.AUTH_BASE_URL + f"/auth/token/validate/?token={token}"
+            headers = {"Authorization": f"Bearer {token}"}
+            response = requests.get(url, headers=headers)
+            return response.status_code == 200
+
+        return False
