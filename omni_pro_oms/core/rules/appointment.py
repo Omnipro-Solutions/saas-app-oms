6409ead4c735a83062ba47dafef54b135d56aaf5
Edwar <edwar.alba@omni.pro>
2024-05-14 09:47:00 -0500

    Refactor post_api method in AppointmentApi to include additional parameters

--- omni_pro_oms/core/rules/appointment.py
+++ omni_pro_oms/core/rules/appointment.py
@@ -5,8 +5,11 @@ class AppointmentApi:
     def __init__(self, api_client: ApiClient):
         self.api_client = api_client
 
-    def post_api(self, endpoint,**kwargs):
+    def post_api(self, endpoint, **kwargs):
         data = self.api_client.call_api(
-            method="POST", endpoint=endpoint, **kwargs
+            method="POST",
+            endpoint=endpoint,
+            raise_status=False,
+            **kwargs,
         )
-        return data.get("result")
+        return data
